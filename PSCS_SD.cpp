a31f87f817f2ae066692881e450051c5aed03ce7
BROWN1213 <s091508@gmail.com>
2018-06-16 20:39:32 +0900

    Update PSCS_SD.cpp

--- PSCS_SD.cpp
+++ PSCS_SD.cpp
@@ -1,3 +1,12 @@
+/**
+* @file PSCS_SD.cpp
+* @author SeungMin Shin, Haneulbit Kim, Chan Lee
+* @license This project is released under the MIT License (MIT)
+* @copyright Copyright (c) 2018 Asgardia
+* @date June 2018
+* @brief ...
+*
+*/
 #include "PSCS_SD.h"
 #include "Arduino.h"
 
